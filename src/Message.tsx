803714523bb278fa24d0dfbd581ec8db6716293a
Jona <jonathan.emmanuel604@gmail.com>
2025-04-30 23:58:22 -0600

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,15 @@
+
+
+function Message()
+{
+    //codigo jsx pasa a js 
+    const name = 'Jona';
+    if(name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World</h1>
+
+    
+}
+
+
+export default Message;
